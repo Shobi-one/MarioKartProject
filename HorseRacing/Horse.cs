ca0d00d466fc684477245ffd66513dc3cb266746
Shobi-one <99034487+Shobi-one@users.noreply.github.com>
2024-02-21 19:32:55 +0100

    Add project files.

---
+++ HorseRacing/Horse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing
+{
+    public class Horse
+    {
+        private string name { get; set; }
+        private int speed { get; set; }
+
+        public Horse(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public static Horse GenerateHorse()
+        {
+            Random random = new Random();
+            string[] possibleNames = { "Thunder", "Blaze", "Swift", "Shadow", "Spirit" }; // Sample horse names will grab them from file in the future
+            string name = possibleNames[random.Next(possibleNames.Length)];
+            int speed = random.Next(5, 15);
+            return new Horse(name, speed);
+        }
+    }
+}
