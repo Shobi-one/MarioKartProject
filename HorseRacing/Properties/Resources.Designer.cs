3e7eaccb19da6bb6e49a1dacf04946db1e181eca
Micah <micah.botha@gmail.com>
2024-03-06 15:10:45 +0100

    Added new image files

--- HorseRacing/Properties/Resources.Designer.cs
+++ HorseRacing/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HorseRacing.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,46 @@ namespace HorseRacing.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bowsercastle_3 {
+            get {
+                object obj = ResourceManager.GetObject("bowsercastle_3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap chocoisland_1 {
+            get {
+                object obj = ResourceManager.GetObject("chocoisland_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap mariocircuit_1 {
+            get {
+                object obj = ResourceManager.GetObject("mariocircuit_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rainbowroad {
+            get {
+                object obj = ResourceManager.GetObject("rainbowroad", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
